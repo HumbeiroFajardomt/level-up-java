c69519a53c31f8158f2f6f6d44731e9a4dcb911a
HumbeiroFajardomt <113655610+HumbeiroFajardomt@users.noreply.github.com>
2022-12-27 16:29:27 +0000

    Even or odd

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -1,13 +1,29 @@
 package com.linkedin.javacodechallenges;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+
+    public static boolean isEven(int number){
+        return number % 2 == 0;
+    }
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        //if number is even, function returns "True"
+        //if number is odd, function returns "False"
+        //The input must be an integer
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int number = input.nextInt();
+        System.out.println("Is the number even?: " + isEven(number));
+        input.close();
+    
     }
 }
+
